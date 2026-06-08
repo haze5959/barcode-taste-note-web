43fbc4d7ea3e67248ac4358ed9aa93c84de39283
kwon ogyu <oqkwon92@gmail.com>
2026-06-08 23:39:30 +0900

    ㄴㅇㄹㄴㄹ

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -233,7 +233,7 @@ export default function Home() {
               draggable={false}
             />
           </motion.div>
-          {[1, 2, 3, 4, 5, 6, 7, 8].map((num) => (
+          {[1, 2, 3, 4, 5, 6].map((num) => (
             <motion.div
               key={num}
               initial="hidden"
